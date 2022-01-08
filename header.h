3f365df82e5b2b48200ab752201ef8a003dc9435
jeongseonwoong <tjsdnd1219@naver.com>
2022-01-09 00:46:34 +0900

    헤더파일

---
+++ header.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include<conio.h>
+#include<time.h>
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+int main();
+int game();
+void CreateEnemy(int);
+void myplane_1(int);
+void Enemy_plane_1();
+void gotoxy(int,int);
+void item(int,int,int,int*);
