c018059dc69b01d7ead77ed2b2f126f69b844d44
jeongseonwoong <tjsdnd1219@naver.com>
2022-02-16 21:16:54 +0900

    왼쪽 상단에 나의 체력 표시, HP가 0이하가 되면 break 문을 이용해 while문 종료

--- game.c
+++ game.c
@@ -6,10 +6,12 @@ void gotoxy(int x, int y)
 	COORD CursorPosition = { x, y }; SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CursorPosition);
 }
 
-void rates(int &count,int &refreshRate)
+void rates(int &count,int &refreshRate,int HP)
 {
 	gotoxy(0, 0);
 	printf("RATE: %d", count);
+	gotoxy(0, 1);
+	printf("HP:%d", HP);
 	if (count % refreshRate == 0)printf("\n%d", count / refreshRate);
 	Sleep(1000 / refreshRate);
 	count++;
@@ -40,17 +42,23 @@ void movement(int &x,int&y,int upgrade,int count)
 }
 
 
-int game()
+
+void game()
 {
 	int refreshRate = 50;
 	int x = 58, y = 13;
-	int count = 1,upgrade=0;
+	int count = 1,upgrade=0,My_Plane_HP=5;
 	while (TRUE)
 	{
 		movement(x,y,upgrade,count);
+		if(MyHp(x, y, upgrade, &My_Plane_HP)<=0)
+		{
+			break;
+		}
 		MyBullet(x,y,upgrade,count);
-		item(count,x,y,&upgrade);
-		CreateEnemy(count);
-		rates(count,refreshRate);
+		item(count,x,y,&upgrade,&My_Plane_HP);
+		CreateEnemy(x, y, count);
+		rates(count,refreshRate,My_Plane_HP);
 	}
+	system("cls");
 }
