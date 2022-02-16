d9e01d8fff4aa9739b9c7371c26ffaa811b680e7
jeongseonwoong <tjsdnd1219@naver.com>
2022-02-16 21:11:02 +0900

    CreateEnemy()인자 추가, 적 비행기 함수 삭제, item()함수 인자 추가

--- header.h
+++ header.h
@@ -8,14 +8,11 @@
 #define UP 72
 #define DOWN 80
 int main();
-int game();
+void game();
 int StartPoint();
-void CreateEnemy(int);
+void CreateEnemy(int,int,int);
+int MyHp(int,int,int,int*);
 void myplane_1(int);
-void Enemy_plane_1(int);
-void Enemy_plane_2(int);
-void Enemy_plane_3(int);
-void Enemy_plane_4(int);
 void gotoxy(int,int);
-void item(int,int,int,int*);
+void item(int,int,int,int*,int*);
 void MyBullet(int,int,int,int);
